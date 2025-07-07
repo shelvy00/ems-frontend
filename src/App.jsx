a72c0e2d9709c76b81315442456444db6fe177c7
Shelvy <shelvy511@gmail.com>
2025-07-06 23:43:44 -0500

    Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import HeaderComponent from './components/HeaderComponent'
 import ListEmpolyeeComponent from './components/ListEmpolyeeComponent'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
 
   return (
     <>
+    <HeaderComponent />
       <ListEmpolyeeComponent />
     </>
   )
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <header></header>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <a className="navbar-brand" href="https://www.javaguides.net">Employee Management System</a>
+      </nav>
+    </div>
+  )
+}
+
+export default HeaderComponent
